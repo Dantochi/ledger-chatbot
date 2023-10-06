09fa07a1e1bda60d541a512008ac5f23062ccd37
Tochukwu <chiditochukwudaniel@gmail.com>
2023-10-06 16:53:02 +0100

    Emergency commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
+import random
+import pickle
+import numpy as np
+import json  # This is required to handle/load the intents created in the intents.json folder
+
+words_collapse = WordNetLemmatizer()  # This is used to collapse similar words to one word such as builds, built,
+# building to build
+# This json.loads() used to convert the json file into format that is readable, editable and accessible by python.
+# From JSON to dictionary but structure is maintained.
+intents = json.loads(open("intent.json").read())
+words = pickle.load(open('words.pkl', 'rb'))  # rb stands for read bytes
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('../chatbot_model.model')
+
+def clean_up_sentence(sentence):  # This function returns the list of words in a particular text without any duplicates
+    # This nltk.word_tokenize() helps to split words, its like the machine reading a text word for word and the
+    # result is used to find trends in text
+    sentence_words = nltk.word_tokenize(sentence)
+    # Using the lemmatize methods help to flush all duplicates of words so that only one of each word remains.
+    sentence_words = [words_collapse.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+        return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    rest = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(rest) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+
+def get_response(intents_list, intents_json):
+    global result
+    tag = intents_list[0]['intents']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['i'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+print("GO! Bot is running!")
+
+while True:
+    message = input("")
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print(res)
+
+
---
+++ training.py
@@ -0,0 +1,71 @@
+import random, pickle, json, nltk
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+from tensorflow import keras
+
+word_collapse = WordNetLemmatizer()  # Collapsing of similar words to one
+intents = json.loads(open('intent.json').read())
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+for intent in intents['intent']:
+    for inputs in intent['input']:
+        word_list = nltk.word_tokenize(inputs)
+        words.append(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+# print(documents)
+# print(words)
+words = [word_collapse.lemmatize(str(word)) for word in words if word not in ignore_letters]
+words = sorted(
+    set(words))  # The set takes out the duplicates but the sorted sorts the element in ascending or descending order
+
+classes = sorted(set(classes))
+pickle.dump(words, open('words.pkl',
+                        'wb'))  # This pickle module helps in serialization or conversion of a python object such as list or arrays into a format that can be easily stored by the computer. As we all know it's the binary format.
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+training = []
+bag_list = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [word_collapse.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)  # So I believe this line is trying to get the frequency of appearance of the words in the array word_patterns
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([output_row])
+    bag_list.append([bag])
+
+random.shuffle(training)
+random.shuffle(bag_list)
+print(training)
+training = np.array(training)
+bag_list = np.array(bag_list)
+
+train_x = list(training[:, 0])
+train_y = list(bag_list[:, 0])
+
+model = keras.models.Sequential()
+model.add(keras.layers.Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(64, activation='relu'))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(len(train_y[0]), activation='softmax'))
+
+sgd = keras.optimizers.SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.keras', hist)
+print('Done')
